2a9832c450dd8cde9993a87504b15d792ae5ee0b
Oleg Gordiyenko <oleg.gordey777@gmail.com>
2016-11-25 12:39:10 +0200

    dell old files

--- rip-html/html/js/skroll_main.js
+++
@@ -1,2 +0,0 @@
-function init(){window.setTimeout(function(){start()},2e3)}function start(){$("body").removeClass("loading").addClass("loaded")}$(document).ready(function(){$(".main").onepage_scroll({sectionContainer:"section",easing:"ease",animationTime:900,pagination:!0,updateURL:!1})}),$(window).load(function(){init()});
-//# sourceMappingURL=maps/skroll_main.js.map
